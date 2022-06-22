d81091b5079cfdcf678bc927431246af8030fe43
x0n4d0 <romagnolimauricio@gmail.com>
2022-06-22 00:44:28 -0300

    feat: ensures that environment variables will be read

--- packages/simulator/main.go
+++ packages/simulator/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/joho/godotenv"
 	route "github.com/x0n4d0/code-delivery-simulator/application/route"
 )
+
+func init() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	route := route.Route{
