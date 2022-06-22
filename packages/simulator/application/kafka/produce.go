cded8b10fd85c3681020a7e54fc6457b4785f88f
x0n4d0 <romagnolimauricio@gmail.com>
2022-06-22 01:13:32 -0300

    feat: add kafka to Produce in application layer

---
+++ packages/simulator/application/kafka/produce.go
@@ -0,0 +1,29 @@
+package kafka
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"time"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	route2 "github.com/x0n4d0/code-delivery-simulator/application/route"
+	"github.com/x0n4d0/code-delivery-simulator/infra/kafka"
+)
+
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route2.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
